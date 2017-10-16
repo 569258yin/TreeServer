135c40d256c7712758d0aceeb98404e107fad09c
haoyin <hao.yin@jinzhimu.com>
2017-10-16 22:41:55 +0800

    add html

---
+++ src/main/java/treeserver/config/WebConfigurer.java
@@ -0,0 +1,18 @@
+package treeserver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by kevinyin on 2017/10/16.
+ */
+@Configuration
+public class WebConfigurer extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+        super.addResourceHandlers(registry);
+    }
+}
